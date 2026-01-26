6e5370f9ac73b6933404fbac1b52709d45903da2
Farras <phfarzation@gmail.com>
2026-01-26 13:31:46 +0800

    Implemented Infinite Loop

--- server.c
+++ server.c
@@ -8,52 +8,65 @@ int main() {
     int c;
     char buffer[2000]; 
 
+    // Initialize Winsock
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
         printf("Failed. Error Code : %d", WSAGetLastError());
         return 1;
     }
 
+    // Create Socket
     server_socket = socket(AF_INET, SOCK_STREAM, 0);
     
+    // Prepare the sockaddr_in structure
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = INADDR_ANY;
     server.sin_port = htons(8080);
 
+    // Bind
     if (bind(server_socket, (struct sockaddr *)&server, sizeof(server)) == SOCKET_ERROR) {
         printf("Bind failed with error code : %d", WSAGetLastError());
         return 1;
     }
     
+    // Listen
     listen(server_socket, 3);
+    printf("Server is running on http://localhost:8080 ...\n");
 
-    printf("Waiting for incoming connections...\n");
-    
-    c = sizeof(struct sockaddr_in);
-    client_socket = accept(server_socket, (struct sockaddr *)&client, &c);
-    
-    recv(client_socket, buffer, 2000, 0);
-    printf("%s\n", buffer); // Print request to terminal
+    // INFINITE LOOP STARTING 
+    while(1) {
+        printf("\nWaiting for incoming connections...\n");
 
-    
-    // 1. Define the Response
-    // We must manually format the HTTP headers. 
-    // \r\n is the standard "Enter" key for the internet.
-    char http_response[] = "HTTP/1.1 200 OK\r\n"
-                           "Content-Type: text/html\r\n" // We are sending HTML now!
-                           "\r\n" // The empty line that separates headers from body
-                           "<html><body>"
-                           "<h1>Hello from C!</h1>"
-                           "<p>This page is being served by a raw socket server.</p>"
-                           "</body></html>";
+        c = sizeof(struct sockaddr_in);
+        client_socket = accept(server_socket, (struct sockaddr *)&client, &c);
+        
+        if (client_socket == INVALID_SOCKET) {
+            printf("accept failed with error code : %d", WSAGetLastError());
+            continue; // Don't kill the server, just skip this connection
+        }
+        
+        printf("Connection accepted!\n");
 
-    // 2. Send the Response
-    send(client_socket, http_response, strlen(http_response), 0);
-    printf("Response sent to browser.\n");
+        // Read the request (we don't do anything with it yet, just clear the buffer)
+        recv(client_socket, buffer, 2000, 0);
 
+        // Send the Response
+        char http_response[] = "HTTP/1.1 200 OK\r\n"
+                               "Content-Type: text/html\r\n"
+                               "\r\n"
+                               "<html><body>"
+                               "<h1>My C Server is Running Forever!</h1>"
+                               "<p>Refresh this page as many times as you want.</p>"
+                               "</body></html>";
 
-    closesocket(client_socket);
+        send(client_socket, http_response, strlen(http_response), 0);
+        printf("Response sent.\n");
+
+        // Close the connection with THIS specific client, but keep the server open
+        closesocket(client_socket);
+    }
+    // LOOP ENDS 
+
     closesocket(server_socket);
     WSACleanup();
-
     return 0;
 }
